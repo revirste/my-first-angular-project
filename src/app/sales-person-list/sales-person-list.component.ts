9f205c59bd8d164a7f3c3ca0cd9705e57912f2c2
Nick Black <nbla139@wgu.edu>
2024-12-23 16:48:00 -0500

    Added an additional column to check if the sales person has met their quota. Also added USD formatting to sales volume.

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -1,21 +1,23 @@
 import { Component } from '@angular/core';
 import { SalesPerson } from './SalesPerson';
-import { NgFor } from '@angular/common';
 import { CommonModule } from '@angular/common';
+import { NgFor } from '@angular/common';
+import { NgIf } from '@angular/common'
+
 
 @Component({
   selector: 'app-sales-person-list',
-  imports: [NgFor, CommonModule],
+  imports: [CommonModule, NgFor, NgIf],
   templateUrl: './sales-person-list.component.html',
   styleUrl: './sales-person-list.component.css'
 })
 export class SalesPersonListComponent {
 
   salesPeopleList: SalesPerson[] = [
-    new SalesPerson("John", "Smith", "jsmith@store.com", 25),
-    new SalesPerson("James", "Doe", "jdoe@store.com", 57),
-    new SalesPerson("Olivia", "Brown", "obrown@store.com", 13),
-    new SalesPerson("Sam", "Greene", "sgreene@store.com", 27)
+    new SalesPerson("John", "Smith", "jsmith@store.com", 250),
+    new SalesPerson("James", "Doe", "jdoe@store.com", 570),
+    new SalesPerson("Olivia", "Brown", "obrown@store.com", 130),
+    new SalesPerson("Sam", "Greene", "sgreene@store.com", 270)
   ]
 
 }
