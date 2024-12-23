fd11657e3fa44d52d8a48f957efd8a3525d35b60
Nick Black <nbla139@wgu.edu>
2024-12-23 15:23:57 -0500

    Added another data member to the app component class to experiment with using variables for rendering content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+  import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-first-angular-project';
+  title = 'This is my first Angular web app!';
+  favoriteFood = 'Tofu'
 }
